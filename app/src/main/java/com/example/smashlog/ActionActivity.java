558d7194286afb489e4dddd3dfeb995d1d66962e
AMillik1 <amillik1@binghamton.edu>
2019-11-15 15:17:23 -0500

    app5 submit

--- app/src/main/java/com/example/smashlog/ActionActivity.java
+++ app/src/main/java/com/example/smashlog/ActionActivity.java
@@ -3,12 +3,30 @@ package com.example.smashlog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ActionActivity extends AppCompatActivity {
+
+    private Spinner spinner1, spinner2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_action);
+
+
+        //character spinners
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.characters_array,
+                android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner1 = findViewById(R.id.spinner);
+        spinner1.setAdapter(adapter);
+        //spinner1.setOnItemSelectedListener(this);
+
+        spinner2 = findViewById(R.id.spinner2);
+        spinner2.setAdapter(adapter);
+        //spinner2.setOnItemSelectedListener(this);
     }
 }
--- app/src/main/java/com/example/smashlog/MainActivity.java
+++ app/src/main/java/com/example/smashlog/MainActivity.java
@@ -39,25 +39,11 @@ import java.util.Collections;
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private static final int RC_SIGN_IN = 007;
     private Account account;
-    private Spinner spinner1, spinner2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //character spinners
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.characters_array,
-                android.R.layout.simple_spinner_dropdown_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        spinner1 = findViewById(R.id.spinner);
-        spinner1.setAdapter(adapter);
-        spinner1.setOnItemSelectedListener(this);
-
-        spinner2 = findViewById(R.id.spinner2);
-        spinner2.setAdapter(adapter);
-        spinner2.setOnItemSelectedListener(this);
     }
 
     @Override
